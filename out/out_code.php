209dd6e5dfe8ec828962a86545e44f855348426a
kaitora <kaitora@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-25 11:19:39 +0000

    removing 1.0 files from trunk ready for 1.1
    git-svn-id: https://plugins.svn.wordpress.org/kaitora-affiliate-system/trunk@350973 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- out/out_code.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<title><?PHP echo $kdals_out_print['keyword'] ; ?></title>
-<meta name="robots" content="noindex,nofollow">
-</head>
-<body>
-<?PHP echo $kdals_out_print['url']; ?>
-</body>
-</html>
