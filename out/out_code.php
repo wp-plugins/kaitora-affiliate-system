2bb768bed975cd792348977d3033929cddd0a6c5
kaitora <kaitora@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-01 14:59:08 +0000

    adding 1.2 files
    git-svn-id: https://plugins.svn.wordpress.org/kaitora-affiliate-system/tags/1.2@353749 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ out/out_code.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title><?PHP echo $kdals_out_print['keyword'] ; ?></title>
+<meta name="robots" content="noindex,nofollow">
+</head>
+<body>
+<?PHP echo $kdals_out_print['url']; ?>
+</body>
+</html>
